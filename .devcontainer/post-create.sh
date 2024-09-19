33a9da93d8a7c7803ea91fe1cb49f24c9a29e561
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-19 18:12:34 +0000

    chore: Clone `shanten-number`

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,10 +3,18 @@
 set -euxo pipefail
 
 sudo apt-get update
-sudo apt-get install -y gnuplot
+sudo apt-get install -y build-essential gnuplot
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
 
 sudo chown vscode:vscode /workspaces
 
 cargo install --force cargo-make
+
+# Clone `shanten-number`.
+pushd /workspaces
+git clone https://github.com/tomohxx/shanten-number
+pushd shanten-number
+tar -xzf data.tar.gz
+popd
+popd
