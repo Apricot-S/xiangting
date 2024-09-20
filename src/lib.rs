0eb6d19affa9e523796e6c3fab36d4f6b74c6085
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-20 20:41:37 +0000

    docs: Add crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@
 // SPDX-License-Identifier: MIT
 // This file is part of https://github.com/Apricot-S/xiangting
 
+//! A library for calculation of deficiency number (a.k.a. xiangting number, 向聴数).
+//!
+//! This is a port of [majiang-core/lib/xiangting.js](https://github.com/kobalab/majiang-core/blob/master/lib/xiangting.js) to Rust.
+//! However, the following differences apply:
+//!
+//! - Calculates the replacement number, which is equal to the deficiency number (a.k.a. xiangting number, 向聴数) plus one.
+//! - Supports both calculations that include and exclude melds (副露) when considering the four tiles in a hand.
+//! - The original algorithm miscalculated the deficiency number in some cases. These errors have been corrected in this port, but the calculation speed has deteriorated as a result.
+//! - Supports three-player mahjong.
+//! - Does not support short hand or long hand.
+
 mod bingpai;
 mod calculate;
 mod constants;
