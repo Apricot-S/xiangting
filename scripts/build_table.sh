dc076a8ab86d83d6928532b19939679ce0ded751
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-29 04:41:54 +0000

    feat: Prepare for build-table implementation

---
+++ scripts/build_table.sh
@@ -0,0 +1,1 @@
+cargo run --bin build-table -- src/standard/shupai_table.rs src/standard/zipai_table.rs
---
+++ src/bin/build_table.rs
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Apricot S.
+// SPDX-License-Identifier: MIT
+// This file is part of https://github.com/Apricot-S/xiangting
+
+fn main() {
+    println!("build table. TODO");
+}
