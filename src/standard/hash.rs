3b483858eee507545ff8259f5b41bbb8b449ed6a
Apricot-S <mahjong_apricot@outlook.jp>
2024-09-29 17:07:07 +0000

    refactor: use enumerate

--- src/standard/hash.rs
+++ src/standard/hash.rs
@@ -7,19 +7,16 @@ use super::zipai_table::ZIPAI_TABLE;
 
 pub(super) fn hash_shupai(single_color_bingpai: &[u8]) -> u32 {
     let mut h: u32 = 0;
-    let mut i: u8 = 0;
     let mut n: u8 = 0;
 
-    for &c in single_color_bingpai {
+    for (i, &c) in single_color_bingpai.iter().enumerate() {
         debug_assert!(i < 9);
         debug_assert!(c <= 4);
         debug_assert!(n + c <= 14);
         n += c;
-        h += SHUPAI_TABLE[i as usize][n as usize][c as usize];
-        i += 1;
+        h += SHUPAI_TABLE[i][n as usize][c as usize];
     }
 
-    debug_assert!(i == 9);
     debug_assert!(n <= 14);
 
     h
@@ -27,19 +24,16 @@ pub(super) fn hash_shupai(single_color_bingpai: &[u8]) -> u32 {
 
 pub(super) fn hash_zipai(zipai_bingpai: &[u8]) -> u32 {
     let mut h: u32 = 0;
-    let mut i: u8 = 0;
     let mut n: u8 = 0;
 
-    for &c in zipai_bingpai {
+    for (i, &c) in zipai_bingpai.iter().enumerate() {
         debug_assert!(i < 7);
         debug_assert!(c <= 4);
         debug_assert!(n + c <= 14);
         n += c;
-        h += ZIPAI_TABLE[i as usize][n as usize][c as usize];
-        i += 1;
+        h += ZIPAI_TABLE[i][n as usize][c as usize];
     }
 
-    debug_assert!(i == 7);
     debug_assert!(n <= 14);
 
     h
