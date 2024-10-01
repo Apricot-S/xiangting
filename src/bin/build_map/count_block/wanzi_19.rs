481fc4e1a65baaf14f88e9a51af4980eb8e03217
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-01 02:03:09 +0000

    feat: Use all wanzi as arguments even in 19m block

--- src/bin/build_map/count_block/wanzi_19.rs
+++ src/bin/build_map/count_block/wanzi_19.rs
@@ -5,8 +5,8 @@
 use super::core::{BlockCountImpl, SingleColorTileFlag};
 use xiangting::standard::core::BlockCount;
 
-pub(in super::super) fn count_19m_block(wanzi_19_bingpai: &[u8; 2]) -> BlockCount {
-    wanzi_19_bingpai
+pub(in super::super) fn count_19m_block(wanzi_bingpai: &[u8; 9]) -> BlockCount {
+    wanzi_bingpai
         .iter()
         .enumerate()
         .fold(
@@ -19,22 +19,24 @@ pub(in super::super) fn count_19m_block(wanzi_19_bingpai: &[u8; 2]) -> BlockCoun
                 four_tiles_gulipai: SingleColorTileFlag::ZERO,
             },
             |mut acc, (i, &n)| {
-                match n {
-                    4 => {
-                        acc.num_mianzi += 1;
-                        acc.num_gulipai += 1;
-                        acc.gulipai.set(i, true);
+                if i == 0 || i == 8 {
+                    match n {
+                        4 => {
+                            acc.num_mianzi += 1;
+                            acc.num_gulipai += 1;
+                            acc.gulipai.set(i, true);
 
-                        acc.four_tiles_gulipai.set(i, true);
+                            acc.four_tiles_gulipai.set(i, true);
+                        }
+                        3 => acc.num_mianzi += 1,
+                        2 => acc.num_duizi += 1,
+                        1 => {
+                            acc.num_gulipai += 1;
+                            acc.gulipai.set(i, true);
+                        }
+                        0 => (),
+                        _ => panic!("There are 5 or more of the same tiles: {} tiles", n),
                     }
-                    3 => acc.num_mianzi += 1,
-                    2 => acc.num_duizi += 1,
-                    1 => {
-                        acc.num_gulipai += 1;
-                        acc.gulipai.set(i, true);
-                    }
-                    0 => (),
-                    _ => panic!("There are 5 or more of the same tiles: {} tiles", n),
                 }
 
                 acc
@@ -50,7 +52,17 @@ mod tests {
     #[test]
     fn count_19m_block_works() {
         let wanzi_bingpai = [4, 0, 0, 0, 0, 0, 0, 0, 2];
-        let r = count_19m_block(&[wanzi_bingpai[0], wanzi_bingpai[8]]);
+        let r = count_19m_block(&wanzi_bingpai);
+        assert_eq!(r.0, 1);
+        assert_eq!(r.1, 0);
+        assert_eq!(r.2, 1);
+        assert_eq!(r.3, 1);
+    }
+
+    #[test]
+    fn count_19m_block_ignore_2m_to_8m() {
+        let wanzi_bingpai = [4, 3, 0, 0, 0, 0, 0, 1, 2];
+        let r = count_19m_block(&wanzi_bingpai);
         assert_eq!(r.0, 1);
         assert_eq!(r.1, 0);
         assert_eq!(r.2, 1);
