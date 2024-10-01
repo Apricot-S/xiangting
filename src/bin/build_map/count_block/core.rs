4148a3cbb3604774de90e45c2b26d68e37b9c283
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-01 02:24:10 +0000

    feat: Separate types by type of tiles (WIP)

--- src/bin/build_map/count_block/core.rs
+++ src/bin/build_map/count_block/core.rs
@@ -4,6 +4,9 @@
 
 use bitvec::prelude::*;
 use xiangting::standard::core::{BlockCount, BlockCountPattern};
+use xiangting::standard::core::{
+    ShupaiBlockCount, ShupaiBlockCountPattern, Wanzi19BlockCount, ZipaiBlockCount,
+};
 
 pub(super) type SingleColorTileFlag = BitArr!(for 9);
 
@@ -47,6 +50,89 @@ pub(super) struct BlockCountPatternImpl {
 impl BlockCountPatternImpl {
     pub(super) fn to_entry(&self) -> BlockCountPattern {
         [self.a.to_entry(), self.b.to_entry()]
+    }
+}
+
+pub(super) struct ShupaiBlockCountImpl {
+    pub(super) num_mianzi: u8,
+    pub(super) num_dazi: u8,
+    pub(super) num_duizi: u8,
+    pub(super) num_gulipai: u8,
+    pub(super) gulipai: SingleColorTileFlag,
+    pub(super) liangmian_ting: SingleColorTileFlag,
+    pub(super) biankanzhang_ting: SingleColorTileFlag,
+    pub(super) shuangpeng_ting: SingleColorTileFlag,
+    pub(super) danqi_ting: SingleColorTileFlag,
+}
+
+impl ShupaiBlockCountImpl {
+    pub(super) fn to_entry(&self) -> ShupaiBlockCount {
+        (
+            self.num_mianzi,
+            self.num_dazi,
+            self.num_duizi,
+            self.num_gulipai,
+            self.gulipai.load(),
+            self.liangmian_ting.load(),
+            self.biankanzhang_ting.load(),
+            self.shuangpeng_ting.load(),
+            self.danqi_ting.load(),
+        )
+    }
+}
+
+pub(super) struct ShupaiBlockCountPatternImpl {
+    pub(super) a: ShupaiBlockCountImpl, // Pattern with the minimum number of isolated tiles
+    pub(super) b: ShupaiBlockCountImpl, // Pattern with the maximum number of melds
+}
+
+impl ShupaiBlockCountPatternImpl {
+    pub(super) fn to_entry(&self) -> ShupaiBlockCountPattern {
+        [self.a.to_entry(), self.b.to_entry()]
+    }
+}
+
+pub(super) struct ZipaiBlockCountImpl {
+    pub(super) num_mianzi: u8,
+    pub(super) num_duizi: u8,
+    pub(super) num_gulipai: u8,
+    pub(super) gulipai: SingleColorTileFlag,
+    pub(super) shuangpeng_ting: SingleColorTileFlag,
+    pub(super) danqi_ting: SingleColorTileFlag,
+}
+
+impl ZipaiBlockCountImpl {
+    pub(super) fn to_entry(&self) -> ZipaiBlockCount {
+        (
+            self.num_mianzi,
+            self.num_duizi,
+            self.num_gulipai,
+            self.gulipai.load(),
+            self.shuangpeng_ting.load(),
+            self.danqi_ting.load(),
+        )
+    }
+}
+
+pub(super) struct Wanzi19BlockCountImpl {
+    pub(super) num_mianzi: u8,
+    pub(super) num_duizi: u8,
+    pub(super) num_gulipai: u8,
+    pub(super) gulipai: SingleColorTileFlag,
+    pub(super) shuangpeng_ting: SingleColorTileFlag,
+    pub(super) danqi_ting: SingleColorTileFlag,
+}
+
+impl Wanzi19BlockCountImpl {
+    pub(super) fn to_entry(&self) -> Wanzi19BlockCount {
+        (
+            self.num_mianzi,
+            self.num_duizi,
+            self.num_gulipai,
+            self.gulipai.load(),
+            self.shuangpeng_ting.load(),
+            self.danqi_ting.load(),
+        )
     }
 }
 
--- src/standard/core.rs
+++ src/standard/core.rs
@@ -14,6 +14,12 @@ pub type ShupaiTable = [[[usize; 5]; 15]; 9];
 // s = 0, 1, 2, 3, 4
 pub type ZipaiTable = [[[usize; 5]; 15]; 7];
 
+// table[i][n][s]
+// i = 0, 1
+// n = 0, 1, ..., 14
+// s = 0, 1, 2, 3, 4
+pub type Wanzi19Table = [[[usize; 5]; 15]; 2];
+
 // block_count.0 : number of mianzi
 // block_count.1 : number of dazi
 // block_count.2 : number of duizi
@@ -26,8 +32,42 @@ pub type BlockCount = (u8, u8, u8, u8, u16, u16);
 // pattern[1] : pattern with the maximum number of melds
 pub type BlockCountPattern = [BlockCount; 2];
 
+// shupai_block_count.0 : number of melds
+// shupai_block_count.1 : number of joints
+// shupai_block_count.2 : number of pairs
+// shupai_block_count.3 : number of isolated tiles
+// shupai_block_count.4 : isolated tiles flag
+// shupai_block_count.5 : open wait flag
+// shupai_block_count.6 : edge & closed wait flag
+// shupai_block_count.7 : dual pair wait flag
+// shupai_block_count.8 : pair wait flag
+pub type ShupaiBlockCount = (u8, u8, u8, u8, u16, u16, u16, u16, u16);
+
+// pattern[0] : pattern with the minimum number of isolated tiles
+// pattern[1] : pattern with the maximum number of melds
+pub type ShupaiBlockCountPattern = [ShupaiBlockCount; 2];
+
+// zipai_block_count.0 : number of melds
+// zipai_block_count.1 : number of pairs
+// zipai_block_count.2 : number of isolated tiles
+// zipai_block_count.3 : isolated tiles flag
+// zipai_block_count.4 : dual pair wait flag
+// zipai_block_count.5 : pair wait flag
+pub type ZipaiBlockCount = (u8, u8, u8, u8, u8, u8);
+
+// wanzi19_block_count.0 : number of melds
+// wanzi19_block_count.1 : number of pairs
+// wanzi19_block_count.2 : number of isolated tiles
+// wanzi19_block_count.3 : isolated tiles flag
+// wanzi19_block_count.4 : dual pair wait flag
+// wanzi19_block_count.5 : pair wait flag
+pub type Wanzi19BlockCount = (u8, u8, u8, u16, u16, u16);
+
 pub type ShupaiMapValue = BlockCountPattern;
 pub type ShupaiMap = Vec<ShupaiMapValue>;
 
 pub type ZipaiMapValue = BlockCount;
 pub type ZipaiMap = Vec<ZipaiMapValue>;
+
+pub type Wanzi19MapValue = BlockCount;
+pub type Wanzi19Map = Vec<Wanzi19MapValue>;
