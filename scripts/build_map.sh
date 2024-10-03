bf018787e3a5263844442b5da9e3238d4fd06076
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-03 07:10:14 +0000

    chore: Add --release flag to build map

--- scripts/build_map.sh
+++ scripts/build_map.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cargo run --bin build-map --features build-map -- \
+cargo run --bin build-map --release --features build-map -- \
     src/standard/shupai_map.rs \
     src/standard/zipai_map.rs \
     src/standard/wanzi_19_map.rs
