db6ca3c8f25bb491c0bd55bda4bdadb9787d0120
Apricot-S <mahjong_apricot@outlook.jp>
2025-01-12 13:04:17 +0000

    refactor: Rewrite hash function using iterator

--- src/standard/hash.rs
+++ src/standard/hash.rs
@@ -7,55 +7,48 @@ use super::wanzi_19_table::WANZI_19_TABLE;
 use super::zipai_table::ZIPAI_TABLE;
 
 pub fn hash_shupai(single_color_bingpai: &[u8]) -> usize {
-    let mut h: usize = 0;
-    let mut n: u8 = 0;
-
-    for (i, &c) in single_color_bingpai.iter().enumerate() {
-        debug_assert!(i < 9);
-        debug_assert!(c <= 4);
-        debug_assert!(n + c <= 14);
-        n += c;
-        h += SHUPAI_TABLE[i][n as usize][c as usize];
-    }
-
-    debug_assert!(n <= 14);
-
-    h
+    let (hash, _) = single_color_bingpai
+        .iter()
+        .enumerate()
+        .fold((0, 0), |(h, n), (i, &c)| {
+            debug_assert!(i < 9);
+            debug_assert!(c <= 4);
+            debug_assert!(n + c <= 14);
+            let updated_n = n + c;
+            let updated_h = h + SHUPAI_TABLE[i][updated_n as usize][c as usize];
+            (updated_h, updated_n)
+        });
+    hash
 }
 
 pub fn hash_zipai(zipai_bingpai: &[u8]) -> usize {
-    let mut h: usize = 0;
-    let mut n: u8 = 0;
-
-    for (i, &c) in zipai_bingpai.iter().enumerate() {
-        debug_assert!(i < 7);
-        debug_assert!(c <= 4);
-        debug_assert!(n + c <= 14);
-        n += c;
-        h += ZIPAI_TABLE[i][n as usize][c as usize];
-    }
-
-    debug_assert!(n <= 14);
-
-    h
+    let (hash, _) = zipai_bingpai
+        .iter()
+        .enumerate()
+        .fold((0, 0), |(h, n), (i, &c)| {
+            debug_assert!(i < 7);
+            debug_assert!(c <= 4);
+            debug_assert!(n + c <= 14);
+            let updated_n = n + c;
+            let updated_h = h + ZIPAI_TABLE[i][updated_n as usize][c as usize];
+            (updated_h, updated_n)
+        });
+    hash
 }
 
 pub fn hash_19m(wanzi_bingpai: &[u8]) -> usize {
-    let mut h: usize = 0;
-    let mut n: u8 = 0;
-
-    let c_1m = wanzi_bingpai[0];
-    debug_assert!(c_1m <= 4);
-    n += c_1m;
-    h += WANZI_19_TABLE[0][n as usize][c_1m as usize];
-
-    let c_9m = wanzi_bingpai[8];
-    debug_assert!(c_9m <= 4);
-    debug_assert!(n + c_9m <= 8);
-    n += c_9m;
-    h += WANZI_19_TABLE[1][n as usize][c_9m as usize];
-
-    debug_assert!(n <= 8);
-
-    h
+    let (hash, _) = wanzi_bingpai
+        .iter()
+        .step_by(8)
+        .enumerate()
+        .fold((0, 0), |(h, n), (i, &c)| {
+            debug_assert!(i == 0 || i == 8);
+            debug_assert!(c <= 4);
+            debug_assert!(n + c <= 8);
+            let index = if i == 0 { 0 } else { 1 };
+            let updated_n = n + c;
+            let updated_h = h + WANZI_19_TABLE[index][updated_n as usize][c as usize];
+            (updated_h, updated_n)
+        });
+    hash
 }
