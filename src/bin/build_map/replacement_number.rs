29441da9ab0bf70039b4fc58f7637bc36a782555
Apricot-S <mahjong_apricot@outlook.jp>
2025-04-19 06:17:54 +0000

    Refactor: Optimize `get_necessary_tiles` function using `filter_map` for improved clarity

--- src/bin/build_map/replacement_number.rs
+++ src/bin/build_map/replacement_number.rs
@@ -79,13 +79,8 @@ fn get_necessary_tiles<const N: usize>(target_hand: &[u8; N], hand: &[u8; N]) ->
         .iter()
         .zip(hand.iter())
         .enumerate()
-        .fold(0u16, |necessary_tiles, (i, (&t, &h))| {
-            if t > h {
-                necessary_tiles | (1 << i)
-            } else {
-                necessary_tiles
-            }
-        })
+        .filter_map(|(i, (&t, &h))| (t > h).then(|| 1 << i))
+        .fold(0u16, |necessary_tiles, bit| necessary_tiles | bit)
 }
 
 pub(super) fn get_shupai_replacement_number(
