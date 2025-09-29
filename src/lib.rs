4ad525ec56d0e185e45ed47f3a7631897a3cf416
Apricot-S <mahjong_apricot@outlook.jp>
2025-09-29 14:48:08 +0900

    feat: Remove v3 API for v4 development

--- src/lib.rs
+++ src/lib.rs
@@ -11,60 +11,6 @@
 //!
 //! - Supports rules that include and exclude melded tiles when determining if a hand contains four identical tiles.
 //! - Supports three-player mahjong.
-//!
-//! # Example
-//!
-//! ```
-//! # use xiangting::calculate_replacement_number;
-//! # use xiangting::InvalidShoupaiError;
-//! # fn main() -> Result<(), InvalidShoupaiError> {
-//! // 123m456p789s11222z
-//! let hand_14: [u8; 34] = [
-//!     1, 1, 1, 0, 0, 0, 0, 0, 0, // m
-//!     0, 0, 0, 1, 1, 1, 0, 0, 0, // p
-//!     0, 0, 0, 0, 0, 0, 1, 1, 1, // s
-//!     2, 3, 0, 0, 0, 0, 0, // z
-//! ];
-//!
-//! let replacement_number = calculate_replacement_number(&hand_14, &None);
-//! assert_eq!(replacement_number?, 0u8);
-//! # Ok(())
-//! # }
-//! ```
 
-#[cfg(not(feature = "build-file"))]
-mod bingpai;
-#[cfg(not(feature = "build-file"))]
-mod calculate;
-#[cfg(not(feature = "build-file"))]
-mod constants;
-#[cfg(not(feature = "build-file"))]
-mod fulu_mianzi;
-#[cfg(not(feature = "build-file"))]
-mod qiduizi;
-#[cfg(not(feature = "build-file"))]
-mod shisanyao;
-#[cfg(not(feature = "build-file"))]
-mod shoupai;
-#[cfg(not(feature = "build-file"))]
-mod standard;
 #[cfg(not(feature = "build-file"))]
 mod tile;
-
-#[cfg(all(test, not(feature = "build-file")))]
-mod test_utils;
-
-#[cfg(not(feature = "build-file"))]
-pub use bingpai::{Bingpai, InvalidBingpaiError};
-#[cfg(not(feature = "build-file"))]
-pub use calculate::{calculate_replacement_number, calculate_replacement_number_3_player};
-#[cfg(not(feature = "build-file"))]
-pub use fulu_mianzi::{ClaimedTilePosition, FuluMianzi, InvalidFuluMianziError};
-#[cfg(not(feature = "build-file"))]
-pub use shoupai::{FuluMianziList, InvalidShoupaiError};
-#[cfg(not(feature = "build-file"))]
-pub use tile::Tile;
-
-#[cfg(feature = "build-map")]
-#[doc(hidden)]
-pub mod standard;
