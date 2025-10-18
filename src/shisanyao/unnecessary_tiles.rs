51d536f332bb0b03a6ecdeec02333c6d5695137e
Apricot-S <mahjong_apricot@outlook.jp>
2025-10-18 23:29:06 +0900

    refactor: Rename 断么九牌 to 中張牌

--- src/shisanyao/unnecessary_tiles.rs
+++ src/shisanyao/unnecessary_tiles.rs
@@ -35,10 +35,10 @@ pub(in super::super) fn calculate_unnecessary_tiles(shoupai: &Shoupai) -> (u8, T
             },
         );
 
-    const DUANYAOJIUPAI_INDICES: [usize; 21] = [
+    const ZHONGZHANGPAI_INDICES: [usize; 21] = [
         1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25,
     ];
-    let discards = DUANYAOJIUPAI_INDICES
+    let discards = ZHONGZHANGPAI_INDICES
         .iter()
         .map(|&i| (i, &shoupai.bingpai()[i]))
         .filter(|(_, count)| **count > 0)
