c9ceb8b76e17bf76cad9f1a0c48f9c0e24ab59b0
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-14 23:04:28 +0000

    test: Add unit tests

--- src/standard/unpack.rs
+++ src/standard/unpack.rs
@@ -65,3 +65,80 @@ pub(super) fn unpack_unnecessary_tiles(pack: &UnnecessaryTilesMapValue) -> Unpac
         ((pack[2] >> (9 * 2)) & 0x01FF) as u16,
     ]
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unpack_replacement_number() {
+        // 23489x : [0, 1, 1, 1, 0, 0, 0, 1, 1]
+        let pack: ReplacementNumberMapValue = 0u32
+            | 0
+            | (1 << 2)
+            | (4 << 5)
+            | (7 << 9)
+            | (1 << 13)
+            | (1 << 15)
+            | (3 << 18)
+            | (6 << 22)
+            | (9 << 26);
+        let unpacked: UnpackedNumbers = [0, 0, 1, 4, 7, 1, 1, 3, 6, 9];
+
+        assert_eq!(unpack_replacement_number(&pack), unpacked);
+    }
+
+    #[test]
+    fn test_unpack_necessary_tiles() {
+        // 23489x : [0, 1, 1, 1, 0, 0, 0, 1, 1]
+        let pack: NecessaryTilesMapValue = [
+            0b000000000 | (0b001000000 << 9) | (0b111111111 << (9 * 2)),
+            0b111111111 | (0b110000000 << 9) | (0b111111111 << (9 * 2)),
+            0b111111111 | (0b111111111 << 9) | (0b111111111 << (9 * 2)),
+        ];
+        let unpacked: UnpackedTiles = [
+            0b000000000,
+            0b000000000,
+            0b001000000,
+            0b111111111,
+            0b111111111,
+            0b110000000,
+            0b111111111,
+            0b111111111,
+            0b111111111,
+            0b111111111,
+        ];
+
+        assert_eq!(unpack_necessary_tiles(&pack), unpacked);
+    }
+
+    #[test]
+    fn test_unpack_unnecessary_tiles() {
+        // 23455689x : [0, 1, 1, 1, 2, 1, 0, 1, 1]
+        let pack: UnnecessaryTilesMapValue = [
+            0b110111110
+                | (0b110110000 << 9)
+                | (0b110110000 << (9 * 2))
+                | ((0b000110000 & 0b111110000) << (9 * 3 - 4)),
+            (0b000110000 & 0b1111)
+                | (0b000000000 << 4)
+                | (0b110101110 << (4 + 9))
+                | (0b110100000 << (4 + 9 * 2)),
+            0b100100000 | (0b000000000 << 9) | (0b000000000 << (9 * 2)),
+        ];
+        let unpacked: UnpackedTiles = [
+            0b110111110,
+            0b110110000,
+            0b110110000,
+            0b000110000,
+            0b000000000,
+            0b110101110,
+            0b110100000,
+            0b100100000,
+            0b000000000,
+            0b000000000,
+        ];
+
+        assert_eq!(unpack_unnecessary_tiles(&pack), unpacked);
+    }
+}
