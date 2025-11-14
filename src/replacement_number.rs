700028bb81744c421a7c272a96f643fdbfc406f7
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-14 06:27:24 +0000

    docs: Fix doc comment

--- src/replacement_number.rs
+++ src/replacement_number.rs
@@ -13,19 +13,6 @@ use crate::tile::TileCounts;
 
 /// Calculates the replacement number (= xiangting number + 1) for a given hand.
 ///
-/// In some rulesets, melded tiles are excluded when checking whether a hand contains
-/// four identical tiles. In others, melded tiles are included in the calculation.
-/// This function allows you to control that behavior via the `fulu_mianzi_list` argument:
-///
-/// - Use `None` if melds are excluded in the ruleset (e.g., Tenhou, Mahjong Soul).
-/// - Provide `Some(&[..])` if melds are included (e.g., World Riichi Championship, M.LEAGUE).
-///
-/// If fewer melds are provided than required for a complete hand,
-/// the missing ones are treated as melds that do not overlap with the tiles in the hand.
-/// For example, with the hand 123p1s, three melds are required.
-/// If only two are given (e.g., \[444p, 777s]), the third is considered to be
-/// a non-overlapping meld, such as 111z.
-///
 /// # Arguments
 ///
 /// * `bingpai` - 兵牌: A reference to a hand excluding melds (a.k.a. pure hand, 純手牌).
@@ -39,7 +26,7 @@ use crate::tile::TileCounts;
 /// # Examples
 ///
 /// ```
-/// # use xiangting::{ClaimedTilePosition, FuluMianzi, calculate_replacement_number};
+/// # use xiangting::{PlayerCount, calculate_replacement_number};
 /// # use xiangting::XiangtingError;
 /// # fn main() -> Result<(), XiangtingError> {
 /// // 123m456p789s11222z
@@ -50,9 +37,23 @@ use crate::tile::TileCounts;
 ///     2, 3, 0, 0, 0, 0, 0, // z
 /// ];
 ///
-/// let replacement_number = calculate_replacement_number(&hand, None);
+/// let replacement_number = calculate_replacement_number(&hand, None, &PlayerCount::Four);
 /// assert_eq!(replacement_number?, 0u8);
+/// # Ok(())
+/// # }
+/// ```
 ///
+/// In some rulesets, melded tiles are excluded when checking whether a hand contains
+/// four identical tiles. In others, melded tiles are included in the calculation.
+/// This function allows you to control that behavior via the `fulu_mianzi_list` argument:
+///
+/// - Use `None` if melds are excluded in the ruleset (e.g., Tenhou, Mahjong Soul).
+/// - Provide `Some(&[..])` if melds are included (e.g., World Riichi Championship, M.LEAGUE).
+///
+/// ```
+/// # use xiangting::{ClaimedTilePosition, FuluMianzi, PlayerCount, calculate_replacement_number};
+/// # use xiangting::XiangtingError;
+/// # fn main() -> Result<(), XiangtingError> {
 /// // 123m1z
 /// let hand: [u8; 34] = [
 ///     1, 1, 1, 0, 0, 0, 0, 0, 0, // m
@@ -68,11 +69,33 @@ use crate::tile::TileCounts;
 ///     FuluMianzi::Kezi(27),
 /// ];
 ///
-/// let replacement_number_wo_melds = calculate_replacement_number(&hand, None);
+/// let replacement_number_wo_melds = calculate_replacement_number(&hand, None, &PlayerCount::Four);
 /// assert_eq!(replacement_number_wo_melds?, 1u8);
 ///
-/// let replacement_number_w_melds = calculate_replacement_number(&hand, Some(&melds_3));
+/// let replacement_number_w_melds =
+///     calculate_replacement_number(&hand, Some(&melds_3), &PlayerCount::Four);
 /// assert_eq!(replacement_number_w_melds?, 2u8);
+/// # Ok(())
+/// # }
+/// ```
+///
+/// If fewer melds are provided than required for a complete hand,
+/// the missing ones are treated as melds that do not overlap with the tiles in the hand.
+/// For example, with the hand 123p1s, three melds are required.
+/// If only two are given (e.g., \[444p, 777s]), the third is considered to be
+/// a non-overlapping meld, such as 111z.
+///
+/// ```
+/// # use xiangting::{ClaimedTilePosition, FuluMianzi, PlayerCount, calculate_replacement_number};
+/// # use xiangting::XiangtingError;
+/// # fn main() -> Result<(), XiangtingError> {
+/// // 123m1z
+/// let hand: [u8; 34] = [
+///     1, 1, 1, 0, 0, 0, 0, 0, 0, // m
+///     0, 0, 0, 0, 0, 0, 0, 0, 0, // p
+///     0, 0, 0, 0, 0, 0, 0, 0, 0, // s
+///     1, 0, 0, 0, 0, 0, 0, // z
+/// ];
 ///
 /// // 456p 7777s
 /// let melds_2 = [
@@ -80,8 +103,33 @@ use crate::tile::TileCounts;
 ///     FuluMianzi::Gangzi(24),
 /// ];
 ///
-/// let replacement_number_w_missing_melds = calculate_replacement_number(&hand, Some(&melds_2));
+/// let replacement_number_w_missing_melds =
+///     calculate_replacement_number(&hand, Some(&melds_2), &PlayerCount::Four);
 /// assert_eq!(replacement_number_w_missing_melds?, 1u8);
+/// # Ok(())
+/// # }
+/// ```
+///
+/// In three-player mahjong, the tiles from 2m (二萬) to 8m (八萬) are not used.
+/// In addition, melded sequences (明順子) are not allowed.
+///
+/// ```
+/// # use xiangting::{PlayerCount, calculate_replacement_number};
+/// # use xiangting::XiangtingError;
+/// # fn main() -> Result<(), XiangtingError> {
+/// // 1111m111122233z
+/// let hand: [u8; 34] = [
+///     4, 0, 0, 0, 0, 0, 0, 0, 0, // m
+///     0, 0, 0, 0, 0, 0, 0, 0, 0, // p
+///     0, 0, 0, 0, 0, 0, 0, 0, 0, // s
+///     4, 3, 2, 0, 0, 0, 0, // z
+/// ];
+///
+/// let replacement_number_4p = calculate_replacement_number(&hand, None, &PlayerCount::Four);
+/// assert_eq!(replacement_number_4p.unwrap(), 2u8);
+///
+/// let replacement_number_3p = calculate_replacement_number(&hand, None, &PlayerCount::Three);
+/// assert_eq!(replacement_number_3p.unwrap(), 3u8);
 /// # Ok(())
 /// # }
 /// ```
@@ -110,82 +158,6 @@ fn calculate_replacement_number_4p(
     Ok([r0, r1, r2].into_iter().min().unwrap())
 }
 
-/// Calculates the replacement number (= xiangting number + 1) for a given hand.
-/// This function is for three-player mahjong.
-///
-/// In three-player mahjong, the tiles from 2m (二萬) to 8m (八萬) are not used.
-/// In addition, melded sequences (明順子) are not allowed.
-///
-/// In some rulesets, melded tiles are excluded when checking whether a hand contains
-/// four identical tiles. In others, melded tiles are included in the calculation.
-/// This function allows you to control that behavior via the `fulu_mianzi_list` argument:
-///
-/// - Use `None` if melds are excluded in the ruleset (e.g., Tenhou, Mahjong Soul).
-/// - Provide `Some(&[..])` if melds are included (e.g., World Riichi Championship, M.LEAGUE).
-///
-/// If fewer melds are provided than required for a complete hand,
-/// the missing ones are treated as melds that do not overlap with the tiles in the hand.
-/// For example, with the hand 123p1s, three melds are required.
-/// If only two are given (e.g., \[444p, 777s]), the third is considered to be
-/// a non-overlapping meld, such as 111z.
-///
-/// # Arguments
-///
-/// * `bingpai` - 兵牌: A reference to a hand excluding melds (a.k.a. pure hand, 純手牌).
-/// * `fulu_mianzi_list` - An `Option` containing a reference to a slice of melds.
-/// * `player_count` - The number of players.
-///
-/// # Errors
-///
-/// Returns [`Err`] if the hand is invalid.
-///
-/// # Examples
-///
-/// ```
-/// # use xiangting::{ClaimedTilePosition, FuluMianzi, calculate_replacement_number_3_player};
-/// # use xiangting::XiangtingError;
-/// # fn main() -> Result<(), XiangtingError> {
-/// // 111m456p789s11222z
-/// let hand: [u8; 34] = [
-///     3, 0, 0, 0, 0, 0, 0, 0, 0, // m
-///     0, 0, 0, 1, 1, 1, 0, 0, 0, // p
-///     0, 0, 0, 0, 0, 0, 1, 1, 1, // s
-///     2, 3, 0, 0, 0, 0, 0, // z
-/// ];
-///
-/// let replacement_number = calculate_replacement_number_3_player(&hand, None);
-/// assert_eq!(replacement_number?, 0u8);
-///
-/// // 111m1z
-/// let hand: [u8; 34] = [
-///     3, 0, 0, 0, 0, 0, 0, 0, 0, // m
-///     0, 0, 0, 0, 0, 0, 0, 0, 0, // p
-///     0, 0, 0, 0, 0, 0, 0, 0, 0, // s
-///     1, 0, 0, 0, 0, 0, 0, // z
-/// ];
-///
-/// // 444p 7777s 111z
-/// let melds_3 = [
-///     FuluMianzi::Kezi(12),
-///     FuluMianzi::Gangzi(24),
-///     FuluMianzi::Kezi(27),
-/// ];
-///
-/// let replacement_number_wo_melds = calculate_replacement_number_3_player(&hand, None);
-/// assert_eq!(replacement_number_wo_melds?, 1u8);
-///
-/// let replacement_number_w_melds = calculate_replacement_number_3_player(&hand, Some(&melds_3));
-/// assert_eq!(replacement_number_w_melds?, 2u8);
-///
-/// // 444p 7777s
-/// let melds_2 = [FuluMianzi::Kezi(12), FuluMianzi::Gangzi(24)];
-///
-/// let replacement_number_w_missing_melds =
-///     calculate_replacement_number_3_player(&hand, Some(&melds_2));
-/// assert_eq!(replacement_number_w_missing_melds?, 1u8);
-/// # Ok(())
-/// # }
-/// ```
 fn calculate_replacement_number_3p(
     bingpai: &TileCounts,
     fulu_mianzi_list: Option<&[FuluMianzi]>,
