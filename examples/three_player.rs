434f1e1e20973a02c901a701bd1e30e201c24da7
Apricot-S <mahjong_apricot@outlook.jp>
2025-11-20 20:39:23 +0000

    docs: Change examples

--- examples/three_player.rs
+++ examples/three_player.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 // This file is part of https://github.com/Apricot-S/xiangting
 
-use xiangting::{PlayerCount, calculate_replacement_number};
+use xiangting::{PlayerCount, calculate_necessary_tiles, calculate_unnecessary_tiles};
 
 fn main() {
     // 1111m111122233z
@@ -13,9 +13,15 @@ fn main() {
         4, 3, 2, 0, 0, 0, 0, // z
     ];
 
-    let replacement_number_4p = calculate_replacement_number(&hand, &PlayerCount::Four).unwrap();
-    assert_eq!(replacement_number_4p, 2u8);
+    let (rn_4p, nt_4p) = calculate_necessary_tiles(&hand, &PlayerCount::Four).unwrap();
+    let (_, ut_4p) = calculate_unnecessary_tiles(&hand, &PlayerCount::Four).unwrap();
+    assert_eq!(rn_4p, 2u8);
+    assert_eq!(nt_4p, 0b0000000_000000000_000000000_000000110); // 23m
+    assert_eq!(ut_4p, 0b0000001_000000000_000000000_000000000); // 1z
 
-    let replacement_number_3p = calculate_replacement_number(&hand, &PlayerCount::Three).unwrap();
-    assert_eq!(replacement_number_3p, 3u8);
+    let (rn_3p, nt_3p) = calculate_necessary_tiles(&hand, &PlayerCount::Three).unwrap();
+    let (_, ut_3p) = calculate_unnecessary_tiles(&hand, &PlayerCount::Three).unwrap();
+    assert_eq!(rn_3p, 3u8);
+    assert_eq!(nt_3p, 0b1111100_111111111_111111111_100000000); // 9m123456789p123456789s34567z
+    assert_eq!(ut_3p, 0b0000001_000000000_000000000_000000001); // 1m1z
 }
