8f4a87550e000044abca29072fb0aaec76a6b872
Apricot S. <152096077+Apricot-S@users.noreply.github.com>
2026-08-17 20:51:14 +0900

    refactor: rename build-file feature to build-map (#171)

--- src/standard.rs
+++ src/standard.rs
@@ -2,38 +2,38 @@
 // SPDX-License-Identifier: MIT
 // This file is part of https://github.com/Apricot-S/xiangting
 
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod common;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod core;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod hash;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod necessary_tiles;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod replacement_number;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod shupai_map;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod shupai_table;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod unnecessary_tiles;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod unpack;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod wanzi_19_map;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod wanzi_19_table;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod zipai_map;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 mod zipai_table;
 
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 pub(super) use necessary_tiles::calculate_necessary_tiles;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 pub(super) use replacement_number::calculate_replacement_number;
-#[cfg(not(feature = "build-file"))]
+#[cfg(not(feature = "build-map"))]
 pub(super) use unnecessary_tiles::calculate_unnecessary_tiles;
 
 #[cfg(feature = "build-map")]
